94286093ff5e55933ec3a00e9a103f27430f8526
Daspe <andreu.astrahancev13@mail.ru>
2021-10-31 14:53:29 +0400

    Задание 1

---
+++ Основы ООП в С#/less_2/less_2/Acc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less_2
+{
+    enum type_acc
+    {
+        Кредит,
+        Накопительный
+    }
+    class Acc
+    {
+        
+        private int _numAcc;
+        private double _balance;
+        private type_acc _typeAcc;
+
+        public Acc(int numAcc, double balance, type_acc typeAcc)
+        {
+            this._numAcc = numAcc;
+            this._balance = balance;
+            this._typeAcc = typeAcc;
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                this._balance = value;
+            }
+        }
+        public int NumAcc
+        {
+            get
+            {
+                return this._numAcc;
+            }
+        }
+        public type_acc TypeAcc
+        {
+            get
+            {
+                return this._typeAcc;
+            }
+        }
+    }
+}
---
+++ Основы ООП в С#/less_2/less_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace less_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acc acc = new(13, 23.54, type_acc.Кредит);
+            Console.WriteLine("Номер счёта: "+Convert.ToString(acc.NumAcc)+"; Баланс: "+ Convert.ToString(acc.Balance) + "; Тип счёта: " + Convert.ToString(acc.TypeAcc)+".");
+        }
+    }
+}
