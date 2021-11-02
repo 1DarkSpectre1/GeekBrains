4554e21fb4acce0200a67b01615345f7cbc02b98
Daspe <andreu.astrahancev13@mail.ru>
2021-11-02 18:43:29 +0400

    задание 2

---
+++ Основы ООП в С#/reverseSTR/reverseSTR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace reverseSTR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "qwerty"; 
+            string reverse = string.Empty;
+            for (int i = str.Length - 1; i > -1; i--)
+                reverse += str[i];
+            Console.WriteLine(reverse);
+        }
+    }
+}
