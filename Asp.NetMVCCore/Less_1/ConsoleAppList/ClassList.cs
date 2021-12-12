0d2f569239cd29e527ed9ec613019bd332c2375f
Daspe <andreu.astrahancev13@mail.ru>
2021-12-12 15:29:29 +0400

    Урок 1

---
+++ Asp.NetMVCCore/Less_1/ConsoleAppList/ClassList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppList
+{
+    class ClassList<T>:List<T>
+    {
+        public T _value { get; set; }
+        public List<T> list = new List<T>();
+        public new void Add(T item)
+        {
+            lock(list)
+                list.Add(item);
+        }
+        public new void Remove(T item)
+        {
+            lock (list)
+                list.Remove(item);
+        }
+
+    }
+}
