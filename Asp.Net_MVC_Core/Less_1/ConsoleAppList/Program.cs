8843e9734f383eb7933ba15618f4b2621f4d7d7e
Daspe <andreu.astrahancev13@mail.ru>
2022-03-28 14:03:51 +0400

    урок 1

---
+++ Asp.Net_MVC_Core/Less_1/ConsoleAppList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleAppList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classlist = new ClassList<string>();
+            var threads = new Thread[10];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() =>
+                {
+                    var thread_id = Environment.CurrentManagedThreadId;
+                    for (int j = 0; j < 100; j++)
+                    {
+                        classlist.Add($"Значение: {j} в потоке номер: {thread_id}");
+                    }
+                });
+                threads[i].Start();
+            }
+        }
+    }
+}
