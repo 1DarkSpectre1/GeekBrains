c1cf243f58ada815eb2650713b4af8a00feaebbe
Daspe <andreu.astrahancev13@mail.ru>
2022-04-18 19:37:27 +0400

    Урок 3

---
+++ Asp.Net_MVC_Core/Lesson3/Lesson3/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Lesson3.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
